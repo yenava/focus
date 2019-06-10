de17b91addddc729407c048edf76178785b21318
Randal <545789177@qq.com>
2019-06-10 23:37:21 +0800

    delete all

--- files/index/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r,s,t],u,_(v,w,x,y,z,A,B,_(),C,_(D,E,F,G,H,_(I,J,K,L),M,null,N,G,O,G,P,Q,R,null,S,T,U,V,W,X,Y,T),Z,_(),ba,_(),bb,_(bc,[])),bd,_(),be,_());}; 
-var b="url",c="index.html",d="generationDate",e=new Date(1560180590326.35),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="chooseValue",o="timeSum",p="login",q="sign_name",r="sign_password",s="login_states",t="come_from",u="page",v="packageId",w="f73f9e1446584081bb0625cb9bb4b268",x="type",y="Axure:Page",z="name",A="index",B="notes",C="style",D="baseStyle",E="627587b6038d43cca051c114ac41ad32",F="pageAlignment",G="near",H="fill",I="fillType",J="solid",K="color",L=0xFFFFFFFF,M="image",N="imageHorizontalAlignment",O="imageVerticalAlignment",P="imageRepeat",Q="auto",R="favicon",S="sketchFactor",T="0",U="colorStyle",V="appliedColor",W="fontName",X="Applied Font",Y="borderWidth",Z="adaptiveStyles",ba="interactionMap",bb="diagram",bc="objects",bd="masters",be="objectPaths";
-return _creator();
-})());
